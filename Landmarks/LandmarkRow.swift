e8cbd778dd7e71e7d1c4bbc70a2248f7da64b979
Vladislav Igielski <igielskv@icloud.com>
2020-07-13 00:02:59 +0200

    Adds Model(s) and Resources, LandmarkRow displays test data.

---
+++ Landmarks/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Manoli on 12/07/2020.
+//  Copyright © 2020 macForce. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50.0, height: 50.0)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
