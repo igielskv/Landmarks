7f849249dcf4a484f1d377f96837fd0fd9998615
Vladislav Igielski <igielskv@icloud.com>
2020-07-14 11:51:53 +0200

    Adopts the Model Object in Landmark Views.

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Manoli on 14/07/2020.
+//  Copyright © 2020 macForce. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
