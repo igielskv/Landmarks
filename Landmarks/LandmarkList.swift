831b4e53daab33f12540044a5e6f8be6dab014bf
Vladislav Igielski <igielskv@icloud.com>
2020-07-14 06:58:28 +0200

    Adds LandmarkList which displays dynamic LandmarkRows with data loaded from json file.

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Manoli on 14/07/2020.
+//  Copyright © 2020 macForce. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarkData) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
