af5bf63b19f8ed7ec50a4d7ab1f3a65a68604af8
Vladislav Igielski <igielskv@icloud.com>
2020-07-15 11:52:49 +0200

    Compose the Home View with single featured landmark on top and scrolling HStacks of landmark categories.

--- Landmarks/Home.swift
+++ Landmarks/Home.swift
@@ -13,15 +13,34 @@ struct CategoryHome: View {
         Dictionary(grouping: landmarkData, by: { $0.category.rawValue })
     }
     
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
+    
     var body: some View {
         NavigationView {
             List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200.0)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationBarTitle(Text("Featured"))
         }
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    
+    var body: some View {
+        landmarks[0].image.resizable()
     }
 }
 
