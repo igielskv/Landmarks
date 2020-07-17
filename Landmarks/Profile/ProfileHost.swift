276bcf9f2d59fac52226de4eab1891d24da098e6
Vladislav Igielski <igielskv@icloud.com>
2020-07-17 09:27:20 +0200

    Adds editing capabilities to user profile view.

--- Landmarks/Profile/ProfileHost.swift
+++ Landmarks/Profile/ProfileHost.swift
@@ -9,11 +9,34 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var mode
+    @EnvironmentObject var userData: UserData
     @State var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20.0) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                if self.mode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
+                
+                Spacer()
+                EditButton()
+            }
+            if self.mode?.wrappedValue == .inactive {
+                ProfileSummary(profile: userData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        self.draftProfile = self.userData.profile
+                    }
+                    .onDisappear {
+                        self.userData.profile = self.draftProfile
+                    }
+            }
         }
         .padding()
     }
@@ -22,5 +45,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(UserData())
     }
 }
